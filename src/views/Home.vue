290bf1e3b296ac7ee2241daff7ddfb68a4ae4a68
hyk339 <hyk339@gmail.com>
2021-10-26 16:06:00 +0900

    inital

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld/> 
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    test() {
+      var var1 = "111111";
+      var var2 = 2;
+      var var3 = [1, 2, 3];
+      console.log(var2);
+    },
+  },
+};
 </script>
