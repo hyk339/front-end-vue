4e6501ce38f2d1cc62114a7543f16b72a2c21231
hyk339 <hyk339@gmail.com>
2021-11-01 15:15:19 +0900

    의존성주입, 참조이용

--- src/router/menu04.js
+++ src/router/menu04.js
@@ -6,5 +6,13 @@ export default[
   {
     path: "/menu04/exam02eventemit",
     component: () => import(/* webpackChunkName: "menu04" */ '../views/menu04/exam02EventEmit')
+  },
+  {
+    path: "/menu04/exam03inject",
+    component: () => import(/* webpackChunkName: "menu04" */ '../views/menu04/exam03Inject')
+  },
+  {
+    path: "/menu04/exam04ref",
+    component: () => import(/* webpackChunkName: "menu04" */ '../views/menu04/exam04Ref')
   }
 ]
---
+++ src/views/menu04/exam03Inject/ChildB/ChildC.vue
@@ -0,0 +1,43 @@
+<!-- 컴포넌트 UI 정의 -->
+<template>
+  <div class="card">
+    <div class="card-header">
+      ChildC
+    </div>
+    <div class="card-body">
+      <p>provideData1:{{provideData1}}</p>
+      <p>provideData2:{{provideData2}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  //컴포넌트의 대표이름(devtools에 나오는 이름)
+  name: "ChildC",
+  //추가하고 싶은 컴포넌트를 등록
+  components:{
+
+  },
+  //컴포넌트에서 사용하는 데이터를 정의
+  data() {
+    return {
+
+    };
+  },
+  //컴포넌트에서 사용하는 메서드
+  methods: {
+    
+  },
+  //부모로부터 전달된 데이터 삽입
+  inject: [
+    "provideData1",
+    "provideData2",
+  ]
+};
+</script>
+
+<!-- 컴포넌트 스타일 정의 -->
+<style scoped>
+
+</style>
