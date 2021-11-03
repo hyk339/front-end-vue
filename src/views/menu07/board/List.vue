dadb5e61ae369a2bf26f35de0ed57198d3fec930
hyk339 <hyk339@gmail.com>
2021-11-03 10:59:49 +0900

    페이징 처리

--- src/views/menu07/board/List.vue
+++ src/views/menu07/board/List.vue
@@ -5,7 +5,43 @@
       게시물 목록
     </div>
     <div class="card-body">
-
+      <div v-if="page != null">
+        <table class="table table-sm table-striped table-bordered">
+          <thead>
+            <tr>
+              <th class="text-center" style="width:70px">번호</th>
+              <th class="text-center">제목</th>
+              <th class="text-center" style="width:90px">글쓴이</th>
+              <th class="text-center" style="width:120px">날짜</th>
+              <th class="text-center" style="width:70px">조회수</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="board in page.boards" :key="board.bno">
+              <td class="text-center" style="width:70px">{{board.bno}}</td>
+              <td>{{board.btitle}}</td>
+              <td class="text-center" style="width:90px">{{board.mid}}</td>
+              <td class="text-center" style="width:120px">{{new Date(board.bdate).toLocaleDateString()}}</td>
+              <td class="text-center" style="width:70px">{{board.bhitcount}}</td>
+            </tr>
+            <tr>
+              <td colspan="5" style="text-align: center;">
+                <button class="btn btn-outline-primary btn-sm mr-1" @click="changePageNo(1)">처음</button>
+                <button class="btn btn-outline-info btn-sm mr-1" v-if="page.pager.groupNo > 1" @click="changePageNo(page.pager.startPageNo-1)">이전</button>
+                <button v-for="pageNo in range(page.pager.startPageNo, page.pager.endPageNo)" :key="pageNo"
+                        @click="changePageNo(pageNo)"
+                        :class="`btn ${(pageNo != page.pager.pageNo)?'btn-outline-success':'btn-danger'} btn-sm mr-1`">{{pageNo}}</button>
+                        
+                <button class="btn btn-outline-info btn-sm mr-1" 
+                        v-if="page.pager.groupNo < page.pager.totalGroupNo"
+                        @click="changePageNo(page.pager.endPageNo+1)"
+                        >다음</button>
+                <button class="btn btn-outline-primary btn-sm" @click="changePageNo(page.pager.totalPageNo)">맨끝</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
  <alert-dialog v-if="alertDialog"
                   :loading="loading"
@@ -33,6 +69,7 @@ export default {
       loading : false,
       alertDialog : false,
       alertDialogMessage : "",
+      page: null
     };
   },
   //컴포넌트에서 사용하는 메서드
@@ -45,6 +82,7 @@ export default {
         console.log(response.data);
         this.loading = false;
         this.alertDialog = false;
+        this.page = response.data;
       })
       .catch(error => {
         if(error.response){
@@ -59,10 +97,17 @@ export default {
         }
         
       });
+    },
+    range(start, end){
+      const arr = [];
+      for(var i=start; i<=end; i++){
+        arr.push(i);
+      }
+      return arr;
     }
   },
   //컴포넌트가 생성될 때 실행되는 훅
-  created(){
+  created(){ //list가 처음 생성될때는 페이지에 보이기 전에 1page 내용을 가져오게 한다.
     var pageNo = this.$route.query.pageNo; //url을 통해서 QueryString으로 넘긴 값을 얻을때
     if(pageNo === "undefined"){
       pageNo = 1;
